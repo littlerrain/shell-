b549bec2d759be7e1d05f071d247466672306bd2
OneT <jiesmileuu@gmail.com>
2017-08-13 17:07:54 +0800

    Update checkword.sh

--- checkword.sh
+++ checkword.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #检查某个词是否为系统自带词典中的
 word=$1
-grep "^$1$" /usr/share/dict/british-english -q
+grep "^$1$" /usr/share/dict/british-english -q  # ^开始 $结束  -q禁止产生任何输出
 if [ $? -eq 0 ];then
 echo $word is a dictionary word
 else
